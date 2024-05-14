79ec45a25708eac064d431eb5a37a85a0e50f020
xaviertrz <xavtarazona@gmail.com>
2024-05-14 01:38:52 -0500

    Hopefully final deploy

--- build.dev.sh
+++
@@ -1,4 +0,0 @@
-# Builds the docker image and pushes it to the GCP container registry
-docker build -t shoepv1:v1 .
-docker image list # Extraer id imagen
-docker run -it -p 8080:8080 <id_imagen>
--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 # Builds the docker image and pushes it to the GCP container registry
 envsubst < app.base.yaml > app.yaml
 
-gcloud builds submit --region=us-central1 --tag us-central1-docker.pkg.dev/winter-field-422515-r6/shoep-repo-gcr/shoepv1:v1
+npm install 
+npm run build
 
-gcloud app deploy --image-url=us-central1-docker.pkg.dev/winter-field-422515-r6/shoep-repo-gcr/shoepv1:v1
+cd client
+npm install
+npm run build
+
+cd ..
+gcloud app deploy
